334d1680bd0ede92fa9c50f37b7d44ecab9a29c0
evenflow0422 <serenauzumcu@protonmail.com>
2026-05-18 21:10:15 +0300

    so close yet so far

--- src/Techstack.jsx
+++ src/Techstack.jsx
@@ -1,12 +1,36 @@
-function List(){
-    const techstack=[
-        {id: 1, category: "Language", items: ["Java","Python","SQL","C#","Javascript","PHP"]},
-        {id: 2, category: "Framework/Library",items: ["Spring Boot","React"]},
-        {id: 3, category: "Database", items: ["MySQL","PostgreSQL","Microsoft SQL Server","MongoDB"]},
-        {id: 4, category: "Tools", items: ["Git","Excel"]},
-        {id: 5, category: "Editing", items:["Photoshop","Premiere Pro","After Effects","Media Encoder"]}
-    ];
-const allItems = techstack.flatMap(tech => tech.items);
+function List() {
+  const techstack = [
+    {
+      id: 1,
+      category: "Language",
+      items: ["Java", "Python", "SQL", "C#", "Javascript", "PHP"],
+    },
+    {
+      id: 2,
+      category: "Framework/Library",
+      items: [
+        "Spring Boot",
+        "Tkinter",
+        "React",
+        "Pandas",
+        "NumPy",
+        "Matplotlib",
+        "Seaborn",
+      ],
+    },
+    {
+      id: 3,
+      category: "Database",
+      items: ["MySQL", "PostgreSQL", "Microsoft SQL Server", "MongoDB"],
+    },
+    { id: 4, category: "Tools", items: ["Git", "Excel"] },
+    {
+      id: 5,
+      category: "Editing",
+      items: ["Photoshop", "Premiere Pro", "After Effects", "Media Encoder"],
+    },
+  ];
+  const allItems = techstack.flatMap((tech) => tech.items);
   return (
     <div id="list">
       <ul>
@@ -17,4 +41,4 @@ const allItems = techstack.flatMap(tech => tech.items);
     </div>
   );
 }
-export default List
+export default List;
