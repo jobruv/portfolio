2681dedd73a8f5697a5ec99d7d3df68cf4d2d041
evenflow0422 <serenauzumcu@protonmail.com>
2026-08-19 22:26:49 +0300

    removed feature

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,14 +3,6 @@ import translations from "./translations.jsx";
 function Header({ lang, setLang }) {
   const t = translations[lang].header;
 
-  const handleCvDownload = () => {
-    const file = `${import.meta.env.BASE_URL}${lang === "tr" ? "cv-tr.pdf" : "cv-en.pdf"}`;
-    const link = document.createElement("a");
-    link.href = file;
-    link.download = lang === "tr" ? "cv-tr.pdf" : "cv-en.pdf";
-    link.click();
-  };
-
   return (
     <header>
       <h1>Jobruv / Serena</h1>
@@ -24,14 +16,6 @@ function Header({ lang, setLang }) {
           </li>
           <li>
             <a href="#project">{t.projects}</a>
-          </li>
-          <li>
-            <button onClick={handleCvDownload}>{t.downloadCv}</button>
-          </li>
-          <li>
-            <button onClick={() => setLang(lang === "en" ? "tr" : "en")}>
-              {lang === "en" ? "Türkçe" : "English"}
-            </button>
           </li>
         </ul>
       </nav>
